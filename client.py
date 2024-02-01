c9361fb2165e49d8d916cde4ec2a2d578cdabf89
Hayden Baldwin <haydenbaldwin9@gmail.com>
2024-02-01 13:19:58 -0500

    Initial Commit

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+
+import settings
+
+
+def client_program():
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((settings.HOST, settings.PORT))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
